5ad38608f4369d19c30d4be7542ae13f8c172fe8
Yu Sheng <zeelichsheng@hotmail.com>
2016-08-17 15:42:00 -0700

    backend: add Redis backend

---
+++ auth-backend/src/main/java/com/ysheng/auth/backend/redis/RedisConfiguration.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 Yu Sheng. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.ysheng.auth.backend.redis;
+
+import java.util.Set;
+
+/**
+ * Defines configuration related to Redis.
+ */
+public class RedisConfiguration {
+
+  // The address of the master node.
+  private String masterAddress;
+
+  // The number of maximum connections.
+  private int maxConnections;
+
+  // The list of sentinels.
+  private Set<String> sentinels;
+
+  // Whether to block when connection is exhausted.
+  private boolean blockWhenExhausted;
+
+  ///
+  /// Getters and Setters.
+  ///
+
+  public String getMasterAddress() {
+    return masterAddress;
+  }
+
+  public void setMasterAddress(String masterAddress) {
+    this.masterAddress = masterAddress;
+  }
+
+  public int getMaxConnections() {
+    return maxConnections;
+  }
+
+  public void setMaxConnections(int maxConnections) {
+    this.maxConnections = maxConnections;
+  }
+
+  public Set<String> getSentinels() {
+    return sentinels;
+  }
+
+  public void setSentinels(Set<String> sentinels) {
+    this.sentinels = sentinels;
+  }
+
+  public boolean isBlockWhenExhausted() {
+    return blockWhenExhausted;
+  }
+
+  public void setBlockWhenExhausted(boolean blockWhenExhausted) {
+    this.blockWhenExhausted = blockWhenExhausted;
+  }
+}
---
+++ auth-backend/src/main/java/com/ysheng/auth/backend/redis/RedisDatabase.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2016 Yu Sheng. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.ysheng.auth.backend.redis;
+
+import com.ysheng.auth.common.backend.Database;
+import com.ysheng.auth.model.database.AuthorizationTicket;
+import com.ysheng.auth.model.database.Client;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Implementation of the backend database based on Redis.
+ */
+public class RedisDatabase implements Database {
+
+  // The Redis client.
+  private RedisClient redisClient;
+
+  /**
+   * Constructs a RedisDatabase object.
+   *
+   * @param redisClient The Redis client.
+   */
+  public RedisDatabase(
+      RedisClient redisClient) {
+    this.redisClient = redisClient;
+  }
+
+  /**
+   * Stores a client object in database.
+   *
+   * @param client The client object to be stored.
+   */
+  public void storeClient(Client client) {
+    throw new NotImplementedException();
+  }
+
+  /**
+   * Finds a client object by client identifier.
+   *
+   * @param clientId The client identifier to be matched.
+   * @return A client object that matches the client identifier.
+   */
+  public Client findClientById(String clientId) {
+    throw new NotImplementedException();
+  }
+
+  /**
+   * Finds a client object by redirect URI.
+   *
+   * @param redirectUri The client redirect URI to be matched.
+   * @return A client object that matches the redirect URI.
+   */
+  public Client findClientByRedirectUri(String redirectUri) {
+    throw new NotImplementedException();
+
+  }
+
+  /**
+   * Stores an authorization ticket object in database.
+   *
+   * @param authorizationTicket The authorization ticket object to be stored.
+   */
+  public void storeAuthorizationTicket(AuthorizationTicket authorizationTicket) {
+    throw new NotImplementedException();
+
+  }
+
+  /**
+   * Finds an authorization ticket object by authorization code.
+   *
+   * @param code The authorization code to be matched.
+   * @return An authorization ticket object that matches the authorization code.
+   */
+  public AuthorizationTicket findAuthorizationTicketByCode(String code) {
+    throw new NotImplementedException();
+  }
+}
